4ad95ea76287e464bca9c1ad4a1b37f859ff1fb1
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2023-09-15 21:13:58 +0200

    fix: fixed models.py and utils.py Fix typos in models.py Fix the functionality to create a record in the association table when the user adds new record(s) to 'badges' and/or 'badge_records'

--- db/models.py
+++ db/models.py
@@ -2,7 +2,14 @@ from __future__ import annotations
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey, Uuid, Table, Column
 from typing import List
-from .utils import now_with_timezone
+from uuid import UUID  # https://docs.sqlalchemy.org/en/20/orm/declarative_tables.html#mapped-column-derives-the-datatype-and-nullability-from-the-mapped-annotation
+
+from datetime import datetime
+from pytz import timezone
+
+
+def now_with_timezone(tz: str = 'Europe/Rome'):
+    return datetime.now(timezone(tz)).strftime('%Y-%m-%d %H:%M:%S%z')
 
 
 class Base(DeclarativeBase):
@@ -12,7 +19,7 @@ class Users(Base):
 
     __tablename__ = 'users'
 
-    id: Mapped[Uuid] = mapped_column(primary_key=True, nullable=False)
+    id: Mapped[UUID] = mapped_column(primary_key=True, nullable=False)
     name: Mapped[str] = mapped_column(nullable=False)
     surname: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(nullable=False)
@@ -21,41 +28,45 @@ class Users(Base):
     updated_at: Mapped[str] = mapped_column(nullable=True)
     deleted_at: Mapped[str] = mapped_column(nullable=True)
 
-badge_readers_badges =  Table('badge_readers_badges', 
-                              Base.metadata,
-                              Column('badge_reader_id', ForeignKey('badge_readers.id'), primary_key=True, nullable=False),
-                              Column('badge_id', ForeignKey('badges.id'), primary_key=True, nullable=False))
+class BadgeReaders_Badges(Base):
 
-class BadgerReaders(Base):
+    __tablename__ = 'badge_readers_badges'
+
+    badge_reader_id: Mapped[UUID] = mapped_column(ForeignKey('badge_readers.id'), primary_key=True, nullable=False)
+    badge_id: Mapped[UUID] = mapped_column(ForeignKey('badges.id'), primary_key=True, nullable=False)
+    badge_reader: Mapped[BadgeReaders] = relationship(back_populates='badges')
+    badge: Mapped[Badges] = relationship(back_populates='badge_readers')
+
+class BadgeReaders(Base):
 
     __tablename__ = 'badge_readers'
 
-    id: Mapped[Uuid] = mapped_column(primary_key=True, nullable=False)
+    id: Mapped[UUID] = mapped_column(primary_key=True, nullable=False)
     ip_address: Mapped[str] = mapped_column(nullable=False)
     location: Mapped[str] = mapped_column(nullable=False)
     created_at: Mapped[str] = mapped_column(nullable=False, default=now_with_timezone)
     updated_at: Mapped[str] = mapped_column(nullable=True)
     deleted_at: Mapped[str] = mapped_column(nullable=True)
-    badges: Mapped[List[Badges]] = relationship(secondary=badge_readers_badges, back_populates='badge_readers')
+    badges: Mapped[List[BadgeReaders_Badges]] = relationship(back_populates='badge_reader')
 
 class Badges(Base):
 
     __tablename__ = 'badges'
 
-    id: Mapped[Uuid] = mapped_column(primary_key=True, nullable=False)
+    id: Mapped[UUID] = mapped_column(primary_key=True, nullable=False)
     code: Mapped[int] = mapped_column(nullable=False)
     created_at: Mapped[str] = mapped_column(nullable=False, default=now_with_timezone)
     updated_at: Mapped[str] = mapped_column(nullable=True)
     deleted_at: Mapped[str] = mapped_column(nullable=True)
-    user_id: Mapped[Uuid] = mapped_column(ForeignKey('users.id'))
-    badge_readers: Mapped[List[BadgerReaders]] = relationship(secondary=badge_readers_badges, back_populates='badges')
-
+    user_id: Mapped[UUID] = mapped_column(ForeignKey('users.id'))
+    badge_readers: Mapped[List[BadgeReaders_Badges]] = relationship(back_populates='badge')
+    
 class Accesses(Base):
 
     __tablename__ = 'accesses'
 
-    id: Mapped[Uuid] = mapped_column(primary_key=True, nullable=False)
+    id: Mapped[UUID] = mapped_column(primary_key=True, nullable=False)
     in_timestamp: Mapped[str] = mapped_column(nullable=True)
     out_timestamp: Mapped[str] = mapped_column(nullable=True)
-    badge_id: Mapped[Uuid] = mapped_column(ForeignKey('badges.id'))
-    badge_reader_id: Mapped[Uuid] = mapped_column(ForeignKey('badge_readers.id'))    
+    badge_id: Mapped[UUID] = mapped_column(ForeignKey('badges.id'))
+    badge_reader_id: Mapped[UUID] = mapped_column(ForeignKey('badge_readers.id'))
