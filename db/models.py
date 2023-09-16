aabba44a9b395c873d803e7b2b536be335907553
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2023-09-16 19:15:25 +0200

    fix: in_timestamp is now nullable and has a default timestamp when data is inserted

--- db/models.py
+++ db/models.py
@@ -66,7 +66,7 @@ class Access(Base):
     __tablename__ = 'accesses'
 
     id: Mapped[UUID] = mapped_column(primary_key=True, nullable=False)
-    in_timestamp: Mapped[str] = mapped_column(nullable=True)
+    in_timestamp: Mapped[str] = mapped_column(nullable=False, default=now_with_timezone)
     out_timestamp: Mapped[str] = mapped_column(nullable=True)
     badge_id: Mapped[UUID] = mapped_column(ForeignKey('badges.id'))
     badge_reader_id: Mapped[UUID] = mapped_column(ForeignKey('badge_readers.id'))
