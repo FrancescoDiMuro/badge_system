e311af9639d30ee33ef8693f50a481dcbf189e9f
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2023-09-17 23:18:29 +0200

    feat: restructured file in order to use API routers

--- main.py
+++ main.py
@@ -1,44 +1,15 @@
-from sqlalchemy.orm import Session
-
-from db.utils import db_connect
-from db.utils import now_with_timezone
-
 from fastapi import FastAPI
-from db.users.utils import read_users, read_user_by_id
+from routers import users
 
-from db.dtos import User
+APP_CONFIG: dict = {}
 
-from uuid import UUID
+app = FastAPI()
 
-
-# Connect to the database
-engine = db_connect(create_metadata=False, echo=True)
-if engine is not None:
-            
-    # Create the session
-    with Session(bind=engine) as session:
-        app = FastAPI()
+app.include_router(users.router)
 
 @app.get('/')
-async def root():
-    return {'message': 'Root directory'}
+async def get_root():
+    return {'message': 'Application root'}
 
-@app.get('/users')
-async def get_users(name_like: str = '%', surname_like: str = '%', email_like: str = '%'):
-    
-    users: list[User] = []
-    
-    db_users = read_users(session, name_like, surname_like, email_like)
 
-    for db_user in db_users:
-        users.append(User(**db_user))
 
-    return users
-
-@app.get('/users/{user_id}')
-async def get_user_by_id(user_id: UUID):
-      
-    db_user = read_user_by_id(session=session, user_id=user_id)
-    if isinstance(db_user, dict):
-        return User(**db_user)
-                
