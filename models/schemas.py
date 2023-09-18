74b364e75b4bdddf68494f3d89ba79c67dcbde9e
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2023-09-18 20:40:08 +0200

    fix: fix schemas for badge_readers

--- models/schemas.py
+++ models/schemas.py
@@ -37,13 +37,13 @@ class Badge(BadgePost):
 class BadgeReaderPost(BaseModel):
     id: UUID | None = None
     ip_address: str
-    location: str
+    location: str    
 
 class BadgeReader(BadgeReaderPost):
     created_at: str
     updated_at: str | None = None
     deleted_at: str | None = None
-    badge_ids: list[UUID]
+    badge_ids: list[UUID] | None = None
 
     class Config:
         from_attributes = True
@@ -51,3 +51,4 @@ class BadgeReader(BadgeReaderPost):
 class BadgeReaderPatch(BaseModel):
     ip_address: str | None = None
     location: str | None = None
+    badge_ids: list[UUID] | None = None
