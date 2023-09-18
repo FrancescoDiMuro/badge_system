376ef305e333fcf5d28ec441eb13e063070ec0f6
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2023-09-18 10:01:56 +0200

    feat: add badge_readers schema for badge_readers endpoint

--- models/schemas.py
+++ models/schemas.py
@@ -8,16 +8,29 @@ class UserPost(BaseModel):
     surname: str
     email: EmailStr
     phone: str
-      
         
 class User(UserPost):
     created_at: str
     updated_at: str | None = None
     deleted_at: str | None = None
 
-    
 class UserPatch(BaseModel):
     name: str | None = None
     surname: str | None = None
     email: EmailStr | None = None
     phone: str | None = None
+
+class BadgeReaderPost(BaseModel):
+    id: UUID | None = None
+    ip_address: str
+    location: str
+
+class BadgeReader(BadgeReaderPost):
+    created_at: str
+    updated_at: str | None = None
+    deleted_at: str | None = None
+    badges: list
+
+class BadgeReaderPatch(BaseModel):
+    ip_address: str | None = None
+    location: str | None = None
