2022b28ffa1335c40da5a29b906582b5c80defa8
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2023-09-18 20:41:20 +0200

    feat: add badge_readers router

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from routers import users
+from routers import users, badge_readers
 
 APP_CONFIG: dict = {}
 
 app = FastAPI()
 
 app.include_router(users.router)
+app.include_router(badge_readers.router)
 
 @app.get('/')
 async def get_root():
