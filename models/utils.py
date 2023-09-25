89daa3a0dc8067f51fb4d26f6453533686ec3ac5
FrancescoDiMuro <dimurofrancesco@virgilio.it>
2023-09-25 21:31:50 +0200

    feat: add function to DRY

--- models/utils.py
+++ models/utils.py
@@ -9,3 +9,15 @@ class Base(DeclarativeBase):
 
 def now_with_timezone(tz: str = 'Europe/Rome'):
     return datetime.now(timezone(tz)).strftime('%Y-%m-%d %H:%M:%S%z')
+
+
+def get_fields_from_model(model) -> dict:
+    '''Get fields from the SQLAlchemy model
+
+    Arguments:
+    - model: SQLAlchemy model to extract the fields from
+
+    Returns:
+    - model_fields (dict): fields extracted from the SQLAlchemy model
+    '''
+    return {k: v for k, v in model.__dict__.items() if not k.startswith('_')}
